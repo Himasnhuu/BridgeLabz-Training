a610f24a781a70cf52813e392aad7a5832dba8f7
Himanshu Patel <patelhimanshu609@gmail.com>
2026-03-31 11:07:19 +0530

    deleting duplicate files

--- scenario-based/aerovigil/InvalidFlightException.java
+++
@@ -1,7 +0,0 @@
-package week3dsa.scenario.aerovigil;
-public class InvalidFlightException extends Exception {
-
-	public InvalidFlightException(String m) {
-		super(m);
-	}
-}
--- scenario-based/hazardanalyzer/RobotSafetyException.java
+++
@@ -1,8 +0,0 @@
-package week3dsa.scenario.hazardanalyzer;
-import java.util.Scanner;
-public class RobotSafetyException extends Exception{
-
-	public RobotSafetyException(String m) {
-		super(m);
-	}
-}
